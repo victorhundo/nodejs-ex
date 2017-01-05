ebebfebde3dffafd14ca71358c36e4c9e282100a
victor <vhfsfox@gmail.com>
2017-01-05 13:36:40 -0300

    Refatorado paginação do Não Ouvo

--- static/naoouvo/js/controllers/mainController.js
+++ static/naoouvo/js/controllers/mainController.js
@@ -2,26 +2,56 @@ angular.module("naoouvo").controller("main", function($scope, $http, $window){
 
     var NaoOuvoFeed = "/naoouvo/feed";
     $scope.Npagination = 4;
+    var count = 0;
     $http.get(NaoOuvoFeed).success(function(data) {
             $scope.feed = data;
-            $scope.nPag = Math.floor($scope.feed.todos.length / $scope.Npagination)
-        	$scope.currentPag = 1;
+            setCurrentFeed($scope.feed.todos, "Todos");
+            setExibition();
     });
 
-    $scope.listInicio = 0;
-    $scope.listFinal = 4;
- 
+    $scope.changeFeed = function(feed, title){
+    	setCurrentFeed(feed, title);
+    	setExibition();
+    }
+
+    var setCurrentFeed = function(feed, title){
+    	count = 0;
+    	$scope.feedTitle = title;
+    	$scope.currentFeed = feed;
+    	$scope.currentPod = count + 1;
+    	$scope.nPod = feed.length;
+    }
+
+    var setExibition = function(){
+    	var array = $scope.currentFeed;
+    	var index = arrayMod(count, array.length);
+    	
+    	exibition = [];
+    	for(i = 0; i < $scope.Npagination; i++){
+    		if(i > array.length - 1)
+    			break;
+    		exibition.push(array[index[i]]);
+    	}
+    	$scope.exibition = exibition;
+    	$scope.currentPod = index[0] + 1;
+    }
+
+    var arrayMod = function(x, y){
+    	index = [];
+    	for(i = 0; i < $scope.Npagination; i++){
+    		index.push(x - y * Math.floor(x / y));
+    		x++
+    	}
+    	return index;
+    }
+
     $scope.nextPag = function(){
-        if($scope.currentPag < $scope.nPag)
-            $scope.currentPag++;
-        else
-            $scope.currentPag = 1;
+    	count++;
+    	setExibition();
     }
 
     $scope.prevPag = function(){
-        if($scope.currentPag > 1)
-            $scope.currentPag--;
-        else
-            $scope.currentPag = $scope.nPag;
+    	count--;
+    	setExibition();
     }
 });
